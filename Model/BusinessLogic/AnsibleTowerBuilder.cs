4869639f5639a247e14e2437c64051c9f51561d1
Alex Kuchta <amkuchta@gmail.com>
2017-08-18 10:27:35 -0400

    AnsibleTowerBuilder 90% implemented

--- Model/BusinessLogic/AnsibleTowerBuilder.cs
+++ Model/BusinessLogic/AnsibleTowerBuilder.cs
@@ -5,6 +5,7 @@ using System.Data.SQLite;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml;
 using Vulnerator.Model.DataAccess;
@@ -20,8 +21,6 @@ namespace Vulnerator.Model.BusinessLogic
         { 
             try
             {
-                if (!Directory.Exists(saveDirectory))
-                { Directory.CreateDirectory(saveDirectory); }
                 if (!DatabaseBuilder.sqliteConnection.State.ToString().Equals("Open"))
                 { DatabaseBuilder.sqliteConnection.Open(); }
                 using (SQLiteCommand sqliteCommand = DatabaseBuilder.sqliteConnection.CreateCommand())
@@ -37,12 +36,32 @@ namespace Vulnerator.Model.BusinessLogic
                             string fileName = string.Format(
                                 "{0}\\{1}.xml", 
                                 saveDirectory, 
-                                sqliteDataReader["Unique_Vulnerability_Identifier"].ToString()
+                                sqliteDataReader["Vulnerability_Group_ID"].ToString()
                                 );
                             using (XmlWriter xmlWriter = XmlWriter.Create(fileName, GenerateXmlWriterSettings()))
                             {
+                                string ruleId = string.Format(
+                                    "{0}r{1}_rule",
+                                    sqliteDataReader["Unique_Vulnerability_Identifier"].ToString(),
+                                    sqliteDataReader["Vulnerability_Version"].ToString());
                                 xmlWriter.WriteStartElement("finding");
-                                
+                                xmlWriter.WriteAttributeString("v_id", sqliteDataReader["Vulnerability_Group_ID"].ToString());
+                                xmlWriter.WriteAttributeString("rule_id", ruleId);
+                                xmlWriter.WriteAttributeString("raw_risk", sqliteDataReader["Raw_Risk"].ToString());
+                                xmlWriter.WriteAttributeString("datetime", string.Empty);
+                                xmlWriter.WriteElementString("title", sqliteDataReader["Vulnerability_Title"].ToString());
+                                xmlWriter.WriteElementString("discussion", sqliteDataReader["Vulnerability_Description"].ToString());
+                                xmlWriter.WriteElementString("check_content", sqliteDataReader["Check_Content"].ToString());
+                                xmlWriter.WriteElementString("fix_text", sqliteDataReader["Fix_Text"].ToString());
+                                xmlWriter.WriteElementString("procedure", string.Empty);
+                                xmlWriter.WriteElementString("output", string.Empty);
+                                xmlWriter.WriteElementString("comments", string.Empty);
+                                xmlWriter.WriteElementString("status", string.Empty);
+                                xmlWriter.WriteStartElement("ccis");
+                                Regex regex = new Regex(Properties.Resources.RegexCciSelector);
+                                foreach (Match match in regex.Matches(sqliteDataReader["CCIs"].ToString()))
+                                { xmlWriter.WriteElementString("cci", match.ToString()); }
+                                xmlWriter.WriteEndElement();
                                 xmlWriter.WriteEndElement();
                             }
                         }
--- ViewModel/SettingsViewModel.cs
+++ ViewModel/SettingsViewModel.cs
@@ -3,6 +3,7 @@ using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Messaging;
 using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -250,6 +251,78 @@ namespace Vulnerator.ViewModel
                         }
                     }
                 }
+            }
+        }
+
+        public RelayCommand BuildAnsibleTowerReportsCommand
+        { get { return new RelayCommand(BuildAnsibleTowerReports); } }
+
+        private void BuildAnsibleTowerReports()
+        {
+            try
+            {
+                //backgroundWorker = new BackgroundWorker();
+                //backgroundWorker.DoWork += buildAnsibleTowerReportsBackgroundWorker_DoWork;
+                //backgroundWorker.RunWorkerAsync();
+                //backgroundWorker.Dispose();
+
+                CommonOpenFileDialog commonOpenFileDialog = new CommonOpenFileDialog();
+                commonOpenFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                commonOpenFileDialog.IsFolderPicker = true;
+                if (commonOpenFileDialog.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    string[] operatingSystems = new string[] { "Red Hat Enterprise Linux 6", "Red Hat Enterprise Linux 7", "Solaris 10", "Solaris 11" };
+                    foreach (string os in operatingSystems)
+                    {
+                        string progressLabel = string.Format("Processing {0}", os);
+                        guiFeedback.SetFields(progressLabel, "Visible", true);
+                        Messenger.Default.Send(guiFeedback);
+                        string tempOs = os;
+                        if (os.Contains("Red Hat"))
+                        { tempOs = "Red Hat"; }
+                        string saveFolder = string.Format("{0}\\{1}", commonOpenFileDialog.FileName, tempOs.Replace(" ", ""));
+                        if (!Directory.Exists(saveFolder))
+                        { Directory.CreateDirectory(saveFolder); }
+                        AnsibleTowerBuilder ansibleTowerBuilder = new AnsibleTowerBuilder();
+                        ansibleTowerBuilder.BuildRoles(os, saveFolder);
+                    }
+                }
+                guiFeedback.SetFields("Ansible Tower report creation complete", "Collapsed", true);
+                Messenger.Default.Send(guiFeedback);
+            }
+            catch (Exception exception)
+            { log.Debug("Exception details:", exception); }
+        }
+
+        private void buildAnsibleTowerReportsBackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                CommonOpenFileDialog commonOpenFileDialog = new CommonOpenFileDialog();
+                commonOpenFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                commonOpenFileDialog.IsFolderPicker = true;
+                if (commonOpenFileDialog.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    string[] operatingSystems = new string[] { "Red Hat", "Solaris 10", "Solaris 11" };
+                    foreach (string os in operatingSystems)
+                    {
+                        string progressLabel = string.Format("Processing {0}", os);
+                        guiFeedback.SetFields(progressLabel, "Visible", true);
+                        Messenger.Default.Send(guiFeedback);
+                        string saveFolder = string.Format("{0}\\{1}", commonOpenFileDialog.FileName, os.Replace(" ", string.Empty));
+                        if (!Directory.Exists(saveFolder))
+                        { Directory.CreateDirectory(saveFolder); }
+                        AnsibleTowerBuilder ansibleTowerBuilder = new AnsibleTowerBuilder();
+                        ansibleTowerBuilder.BuildRoles(os, saveFolder);
+                    }
+                }
+                guiFeedback.SetFields("Ansible Tower report creation complete", "Collapsed", true);
+                Messenger.Default.Send(guiFeedback);
+            }
+            catch (Exception exception)
+            {
+                log.Error(string.Format("Unable to generate Ansible Tower report templates."));
+                throw exception;
             }
         }
     }
