ed104b118ec0f24898f62edcb90dbe0cfb84e6c7
Alex Kuchta <amkuchta@gmail.com>
2017-09-19 22:25:48 -0400

    SQLite DateTimes are not parsing into EF correctly...

--- Model/DataAccess/DatabaseBuilder.cs
+++ Model/DataAccess/DatabaseBuilder.cs
@@ -13,7 +13,7 @@ namespace Vulnerator.Model.DataAccess
     {
         private Assembly assembly = Assembly.GetExecutingAssembly();
         public static string databaseConnection = string.Format(
-            @"Data Source = {0}; Version=3;datetimeformat=Ticks;", 
+            @"Data Source = {0}; Version=3;datetimeformat=UnixEpoch;datetimekind=Utc;", 
             Properties.Settings.Default.Database);
         private static readonly ILog log = LogManager.GetLogger(typeof(Logger));
         public static SQLiteConnection sqliteConnection = new SQLiteConnection(databaseConnection);
