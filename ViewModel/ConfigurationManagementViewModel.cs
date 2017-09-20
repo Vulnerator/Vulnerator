13a2ea7b9a10b8a0c0c75966fb9d8a8a98c45662
Alex Kuchta <amkuchta@gmail.com>
2017-09-20 11:42:18 -0400

    Revert "Dispose ObjectContext after UI list population"
    This reverts commit c25e37667e6c96ceb8170c01e2a870a86ae4e11c.

--- ViewModel/ConfigurationManagementViewModel.cs
+++ ViewModel/ConfigurationManagementViewModel.cs
@@ -14,6 +14,8 @@ namespace Vulnerator.ViewModel
 {
     public class ConfigurationManagementViewModel : ViewModelBase
     {
+        private DatabaseContext databaseContext;
+
         private List<Hardware> _hardwares;
         public List<Hardware> Hardwares
         {
@@ -100,22 +102,13 @@ namespace Vulnerator.ViewModel
 
         public ConfigurationManagementViewModel()
         {
-            using (DatabaseContext databaseContext = new DatabaseContext())
-            {
-                Hardwares = databaseContext.Hardwares
-                    .Include(h => h.SoftwareHardwares)
-                    .Include(h => h.IP_Addresses)
-                    .Include(h => h.MAC_Addresses)
-                    .Include(h => h.Groups)
-                    .Include(h => h.Contacts)
-                    .Include(h => h.Hardware_PPS)
-                    .AsNoTracking().ToList();
-                Softwares = databaseContext.Softwares.AsNoTracking().ToList();
-                Contacts = databaseContext.Contacts.AsNoTracking().ToList();
-                PPS = databaseContext.PPS.AsNoTracking().ToList();
-                Groups = databaseContext.Groups.AsNoTracking().ToList();
-                Accreditations = databaseContext.Accreditations.AsNoTracking().ToList();
-            }
+            databaseContext = new DatabaseContext();
+            Hardwares = databaseContext.Hardwares.ToList();
+            Softwares = databaseContext.Softwares.ToList();
+            Contacts = databaseContext.Contacts.ToList();
+            PPS = databaseContext.PPS.ToList();
+            Groups = databaseContext.Groups.ToList();
+            Accreditations = databaseContext.Accreditations.ToList();
         }
     }
 }
