260362c34be48b41ab0f5637fdde28af1bb55877
Alex Kuchta <amkuchta@gmail.com>
2017-12-16 20:36:09 -0500

    Begin implementation of HeaderedTextBox Style

---
+++ View/ViewHelper/DependencyProperties.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Vulnerator.View.ViewHelper
+{
+    class DependencyProperties : TextBlock
+    {
+        public static readonly DependencyProperty UrlProperty = DependencyProperty.RegisterAttached(
+            "Url",
+            typeof(string),
+            typeof(DependencyProperties),
+            new FrameworkPropertyMetadata("")
+            );
+
+        public static void SetUrl(UIElement element, string value)
+        { element.SetValue(UrlProperty, value); }
+
+        public static string GetUrl(UIElement element)
+        { return (string)element.GetValue(UrlProperty); }
+
+        public static readonly DependencyProperty FloatingWatermarkProperty = DependencyProperty.RegisterAttached(
+              "FloatingWatermark", 
+              typeof(string), 
+              typeof(DependencyProperties),
+              new PropertyMetadata(""));
+
+        public static string GetFloatingWatermark(DependencyObject d)
+        { return (string)d.GetValue(FloatingWatermarkProperty); }
+
+        public static void SetFloatingWatermark(DependencyObject d, string value)
+        { d.SetValue(FloatingWatermarkProperty, value); }
+    }
+}
