ba1c89d88b8871209911e5a1c46146772fb2c892
Alex Kuchta <amkuchta@gmail.com>
2018-01-24 15:03:08 -0500

    Moves styles to dedicated files; begins creation of HeaderedComboBox; adjusts EmailValidation to clear when Text.Length = 0

--- View/Validation/EmailValidationRule.cs
+++ View/Validation/EmailValidationRule.cs
@@ -8,7 +8,7 @@ namespace Vulnerator.View.Validation
         public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
         {
             string email = value.ToString();
-            if (!Regex.IsMatch(email, @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
+            if (email.Length > 0 && !Regex.IsMatch(email, @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
             {
                 return new ValidationResult(false, "Please enter a valid email address.");
             }
