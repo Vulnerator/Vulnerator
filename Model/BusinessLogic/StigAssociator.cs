2c7dd61b097d5a9a8e522daafd1eb4553839ced4
Alex Kuchta <amkuchta@gmail.com>
2018-02-18 00:12:38 -0500

    StigAssociator updates (minor) - more GPG signing testing

--- Model/BusinessLogic/StigAssociator.cs
+++ Model/BusinessLogic/StigAssociator.cs
@@ -3,11 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vulnerator.Model.Object;
+using Vulnerator.Model.DataAccess;
+using log4net;
+using System.Data.SQLite;
 
 namespace Vulnerator.Model.BusinessLogic
 {
     public class StigAssociator
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Logger));
+        private DatabaseInterface databaseInterface = new DatabaseInterface();
 
+        public void StigToHardware(int vulnerabilitySourceId)
+        {
+            try
+            {
+                if (!DatabaseBuilder.sqliteConnection.State.Equals("Open"))
+                { DatabaseBuilder.sqliteConnection.Open(); }
+                using (SQLiteTransaction sqliteTransaction = DatabaseBuilder.sqliteConnection.BeginTransaction())
+                {
+                    using (SQLiteCommand sqliteCommand = DatabaseBuilder.sqliteConnection.CreateCommand())
+                    {
+
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                log.Error(string.Format(""));
+                throw exception;
+            }
+        }
     }
 }
