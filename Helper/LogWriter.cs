13632e13f0f628ec7fcf867f65f78a4b3168b62c
Alex Kuchta <amkuchta@gmail.com>
2019-12-15 21:47:17 -0500

    Creates LogWriter.cs & updates MainViewModel.cs to use new Logger class
    - LogWriter simplifies logging with WriteLog Method (+1 overflow)
    - MainViewModel.cs now references the new Logger class in Vulnerator.Helper

---
+++ Helper/LogWriter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using log4net;
+
+namespace Vulnerator.Helper
+{
+    /// <summary>
+    /// Class to handle writing logs for the application
+    /// </summary>
+    public class LogWriter
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Model.Object.Logger));
+
+        /// <summary>
+        /// Log writer to handle 'Error' level logging
+        /// </summary>
+        /// <param name="message">The 'Error' message to be logged</param>
+        public void LogError(string message)
+        {
+            try
+            { log.Error(message); }
+            catch (Exception exception)
+            {
+                #if DEBUG
+                throw exception;
+                #endif
+            }
+        }
+
+        /// <summary>
+        /// Log writer to handle 'Error' and 'Debug' level logging
+        /// </summary>
+        /// <param name="message">The 'Error' message to be logged</param>
+        /// <param name="exception">The Exception to be logged in the 'Debug' output</param>
+        private void LogErrorWithDebug(string message, Exception ex)
+        {
+            try
+            {
+                log.Error(message);
+                log.Debug($"Exception Details: {ex}");
+            }
+            catch (Exception exception)
+            {
+                #if DEBUG
+                throw exception;
+                #endif
+            }
+        }
+    }
+}
