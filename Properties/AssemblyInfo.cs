6aba264159359fdad7a11ee151f87dac08e6e288
Alex Kuchta <amkuchta@gmail.com>
2020-08-17 15:56:57 -0400

    update assembly info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.2.0.*")]
-[assembly: AssemblyFileVersion("6.2.0")]
+[assembly: AssemblyVersion("7.0.0.*")]
+[assembly: AssemblyFileVersion("7.0.0")]
